100fd2c7c19e01b250a1d29a3f177b978babe4aa
Y.Mo <46513398+YJ-Mo@users.noreply.github.com>
2021-06-08 19:35:26 +0800

    Update fs.R

--- fs.R
+++ fs.R
@@ -10,64 +10,48 @@ library("optparse")
 library("pROC")
 library("reshape2")
 library("tidyr")
+library("optparse")
+option_list <- list( 
+    make_option("--count", help="path of raw count matrix"),
+    make_option("--label", help = "path of label file"),
+    make_option("--classifier", help = "path of classifier file"),
+    make_option("--fold", help = "number of cross validation fold"),
+    make_option("--iteration", help = "number of iterations"),
+    make_option("--partition", help = "number of partition times of discovery count matrix"),
+    make_option("--plot_innerAUC", help = "path of saved AUC plot of inner CV")
+)
+opt <- parse_args(OptionParser(option_list=option_list))
+source(opt$classifier)
 
-### Step1: Input count matrix file and label file
-source("/home/user_25/cancer/featurebin/Classifiers.R")
-Combined=read.table("/home/user_25/cancer/rawData/pico.txt",sep='\t',header=TRUE,row.names=1)
-Input=read.table("/home/user_25/cancer/rawData/pico_label.txt",sep='\t',header=TRUE)
-Features.CVparam<- trainControl(method = "repeatedcv",number = 8, repeats =2,verboseIter=TRUE,returnData=FALSE,classProbs = TRUE,savePredictions=FALSE)
+### Step1: Input discovery count matrix file and label file
+DiscoveryCount=read.table(opt$count,sep='\t',header=TRUE,row.names=1)
+Labels=read.table(opt$label,sep='\t',header=TRUE)
 
-Cluster <-makeCluster(5)
-Cluster <- registerDoParallel(Cluster)
-AllIterations.onevEach <- list()
-Splits<-SplitFunction(Combined,Input$label)
-for(i in 1:100) {
-  AllIterations.onevEach[[i]] <- OnevsEach(Combined, classes.df = Splits$df, Indices = Splits$samples[[i]], nDMR = 300) 
-  message(i)
+### Step2: Split discovery count matrix
+Features.CVparam=trainControl(method="repeatedcv",number=opt$fold, repeats=opt$iteration,verboseIter=TRUE,returnData=FALSE,classProbs=TRUE,savePredictions=FALSE)
+Cluster=makeCluster(5)
+Cluster=registerDoParallel(Cluster)
+DiscoveryIteration=list()
+Splits=mSplit(DiscoveryCount,Labels$label,opt$partition)
+for(i in 1:opt$partition) {
+  DiscoveryIteration[[i]]=OnevsEach(DiscoveryCount,nDE=200,classes.df=Splits$df,Indices=Splits$samples[[i]]) 
   cat("Finish",i,"at",date())
 }
-save(AllIterations.onevEach, file = "/home/user_25/cancer/featurebin/AllIterations_300Features_OnevEach.RData")
-PredFunction <- function(ModelList, TestData, Indices, classes.df) { 
-  TrainPheno <- classes.df[Indices,]
-  TestData <- TestData[,!colnames(TestData) %in% TrainPheno$ID]
-  TestPheno <- classes.df%>%filter(!ID %in% TrainPheno$ID)
-  
-  Predictions.list <- list()
-  OutputNames <- names(ModelList)
-  
-  for(i in 1:length(ModelList)) {
-    Features <- ModelList[[i]]$Model$finalModel$xNames
-    TestDataNew <- TestData[match(Features,rownames(TestData)),]
-    Model <- ModelList[[i]]$Model
-    Prediction.classProbs <- predict(Model, newdata = t(TestDataNew), type = "prob")%>%
-      data.frame
-    Prediction.classProbs$ActualClass <- TestPheno$Classes
-    Prediction.classProbs$PredictedClass <- predict(Model, newdata = t(TestDataNew), type = "raw")
-    Predictions.list[[i]] <- Prediction.classProbs
-    message(i)
-  }
-  
-  names(Predictions.list) <- OutputNames
-  return(Predictions.list)
-} 
 
-
+### Step3: Cross validation on splited matrix
 Classes.df <- Splits$df
 TestPerformance.list <- list()
 for(i in 1:100) {
-  TestPerformance.list[[i]] <- PredFunction(ModelList = AllIterations.onevEach[[i]],
-                                            TestData = Combined, Indices = Splits$samples[[i]], classes.df = Classes.df)
-}
+    TestPerformance.list[[i]]=mPredict(ModelList=DiscoveryIteration[[i]],TestData=DiscoveryCount,Indices=Splits$samples[[i]], classes.df = Classes.df)}
 AUCs.DiscoveryCohort <- GetAUC.ClassWise2(TestPerformance.list)
-Counts.Samples <- count(Input, label)%>%
-  mutate(Frac = n/373)
+Counts.Samples <- count(Labels,label) %>% mutate(Frac = n/373)
 
-png(file="/home/user_25/cancer/featurebin/plot.png",width=1000,height=600)
-Plot1 <- qplot(data = AUCs.DiscoveryCohort, y = AUC, x = ID, geom = "jitter", size = I(2), colour = I("orange"))+
-  geom_boxplot(colour = I("black"), alpha = I(0))+
+### Step4: Ploting inner AUC
+png(file=opt$plot_innerAUC)
+Plot_innerAUC=qplot(data=AUCs.DiscoveryCohort,y=AUC,x=ID,geom="jitter",size=I(2), colour=I("#FF6666"))+
+  geom_boxplot(colour=I("black"), alpha=I(0))+
   theme_bw()+
-  ylab("Discovery Cohort AUCs")+
-  xlab("Class")
+  ylab("AUCs within Discovery")+
+  xlab("Cancer types")
+Plot_innerAUC
 dev.off
-Plot1
-
