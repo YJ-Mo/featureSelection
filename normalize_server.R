4d1e4571b5ba7b98810992f3008ee387f08d50f2
Y.Mo <46513398+YJ-Mo@users.noreply.github.com>
2021-06-08 09:38:56 +0800

    Update normalize_server.R

--- normalize_server.R
+++ normalize_server.R
@@ -19,6 +19,7 @@ labels=read.csv(opt$label,sep="\t",header=TRUE,stringsAsFactors = FALSE,check.na
 ref=read.csv(opt$ref,sep = '\t',header = TRUE,stringsAsFactors = FALSE,check.names = FALSE)
 sampleIDs=colnames(count_matrix)
 featureIDs=rownames(count_matrix)
+cat("Finish input at",date())
 
 ################################################
 ####    Step_2: Cross check sample names    ####
@@ -29,6 +30,7 @@ uni_label=unique(labels$label)
 class=factor(labels$label)
 group=as.factor(labels$label)
 count_matrix=count_matrix[,sampleIDs]
+cat("Finish cross check names at",date())
 
 ################################################
 ####     Step_3: Cross check gene names     ####
@@ -36,15 +38,16 @@ count_matrix=count_matrix[,sampleIDs]
 #fea_vec=c()
 #for (fea in featureIDs){
 #  if (TRUE %in% str_detect(fea, ref$`Gene stable ID`)){ fea_vec=append(fea_vec,fea) }}
-
-count_matrix=subset(count_matrix, rownames(count_matrix) %in% fea_vec)
+# count_matrix=subset(count_matrix, rownames(count_matrix) %in% fea_vec)
+cat("Finish cross check genes at",date())
 
 ################################################
 ####         Step_4: Filter sparsity        ####
 ################################################
-#for (i in 1: length(uni_label)){
-#  filter <- apply(count_matrix[,grep(uni_label[i], colnames(count_matrix))], 1, function(x) length(x[x>0])/length(x)>=0.2)
-#  count_matrix <- count_matrix[filter,]}
+for (i in 1: length(uni_label)){
+  filter <- apply(count_matrix[,grep(uni_label[i], colnames(count_matrix))], 1, function(x) length(x[x>0])/length(x)>=0.2)
+  count_matrix <- count_matrix[filter,]}
+cat("Finish filtering sparsity at",date())
 
 ################################################
 ####     Step_5: Differential Analysis      ####
@@ -59,12 +62,14 @@ lrt=glmLRT(fit, coef=2:6)
 top=topTags(lrt, n=nrow(count_matrix))$table
 n.mask=as.integer(0.8*nrow(as.matrix(count_matrix)))
 empirical=rownames(count_matrix)[which(!(rownames(count_matrix) %in% rownames(top)[1:n.mask]))]
+cat("Finish DE at",date())
 
 ################################################
 ####          Step_6: Remove Batch          ####
 ################################################
 res=RUVg(x=log(count_matrix.tmm+0.1), cIdx=empirical, k=2,isLog = T)
 count_matrix.tmm.ruv=exp(res$normalizedCounts)
+cat("Finish removing batch at",date())
 
 ################################################
 ####             Step_7: Output             ####
@@ -72,3 +77,5 @@ count_matrix.tmm.ruv=exp(res$normalizedCounts)
 write.table(count_matrix.tmm,opt$tmm,quote=F,sep="\t")
 write.table(count_matrix.tmm.ruv,opt$ruv,quote=F,sep="\t")
 write.table(top,opt&anova,quote=F,sep="\t")
+cat("Finish output at",date())
+cat("Finish all at",date())
